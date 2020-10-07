2bbc6fa6e6118b3b73409c5dba9965ccf1ae8bfc
vivien <VivienECE>
2020-10-07 03:00:56 -0700

    Part 2 DB

--- test/users.js
+++ test/users.js
@@ -27,7 +27,8 @@ describe('users', () => {
     .get('/users')
     .expect(200)
     users.should.match([{
-      id: /^\w+-\w+-\w+-\w+-\w+$/,
+      //id: /^\w+-\w+-\w+-\w+-\w+$/,
+      id: /^users:\w+-\w+-\w+-\w+-\w+$/,
       username: 'user_1'
     }])
   })

3d643b65b34254bd99459f1fd3bd9d64f11ae99d
VivienECE <vivien.detournay@edu.ece.fr>
2020-10-26 21:55:13 +0100

    front-end

--- front-end/my-cool-app/src/App.js
+++ front-end/my-cool-app/src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([{
+    content : 'First message'
+  },{
+    content : 'Second message'
+  }])
+  const addMessage = (e) => {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    setMessages([
+      ...messages,
+      {content: data.get('content')}
+    ])
+    e.target.elements.content.value = ''
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+       header
       </header>
-    </div>
+      <main className="App-main" onSubmit={addMessage}>
+      <div class="row">
+      <div class='col'>
+        Channels
+      </div>
+        <div class='col'>
+          Messages
+          <ul>
+            {messages.map((message,i) => (
+              <li key={i}>{message.content}</li>
+            ))}
+          </ul>
+          <form>
+              <input type="input" name="content" />
+              <input type="submit" value="Send"/>
+          </form>
+        </div>
+
+      </div>
+    </main>
+  </div>
   );
 }
 
